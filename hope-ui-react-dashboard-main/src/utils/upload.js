651ca2cf97b7e78b594b85577134ff3352dcd52e
YassineAzamiIdrissi <yassine.elazamielidrissi@usmba.ac.ma>
2024-03-18 13:35:35 +0000

    a

--- hope-ui-react-dashboard-main/src/utils/upload.js
+++ hope-ui-react-dashboard-main/src/utils/upload.js
@@ -5,18 +5,15 @@
 //   });
 
 import axios from "axios";
-
 export const upload = async (file) => {
   const data = new FormData();
   data.append("file", file);
   data.append("upload_preset", "fiverr");
-
   try {
     const res = await axios.post(
       "https://api.cloudinary.com/v1_1/dg9qkizkk/image/upload",
       data
     );
-
     const { url } = res.data;
     return url;
   } catch (err) {
